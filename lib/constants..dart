5e93ef5eee6d38580216629a3544cd737cfe58af
hoangkhoa <nhk9200@gmail.com>
2021-04-23 18:07:40 +0700

    first setup

---
+++ lib/constants..dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const pTextColorGray2 = Color(0xFF535353);
+const pTextColorGray3 = Color(0x828282);
+const pItemColor = Color(0xDA7D6);
+
+const pItemOnColor = Color(0X6FCF97);
+const pItemOffColor = Color(0xEB5757);
+
+const pBackGroundColor = Color(0xA4EBF3);
+
+const pAttributeHighColor = Color(0xEB5757);
+const pAttributeLowColor = Color(0x219653);
+const pAttributeMediumColor = Color(0xF2C94C);
+
+const kDefaultPaddin = 20.0;
