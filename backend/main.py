524137f0bcbef80b9e85f95e03a08f148e0b313e
= <akatekhanh0212@gmail.com>
2021-05-10 14:48:08 +0700

    readme for docker and prototype for route

--- backend/main.py
+++ backend/main.py
@@ -8,3 +8,38 @@ application = Flask(__name__)
 @application.route("", methods=['GET'])
 def home():
     return "Hello!"
+
+# Led device
+@application.route("/led")
+def process_led_device():
+    pass
+
+
+# DHT11
+@application.route("/dht11")
+def process_dht11_device():
+    pass
+
+
+# Light sensor
+@application.route("light-sensor")
+def process_light_sensor_device():
+    pass
+
+
+# Buzzer speaker
+@application.route("/buzzer-speaker")
+def process_buzzer_speaker_device():
+    pass
+
+
+# Pump mini
+@application.route("/pump-mini")
+def process_pump_mini_device():
+    pass
+
+
+# Relay circuit
+@application.route('/relay-circuit')
+def process_relay_circuit_device():
+    pass
---
+++ backend/readme.md
@@ -0,0 +1,12 @@
+# Build docker image
+
+1. Pull docker repository
+   docker pull mongo
+2. Create directory to mapping from local to container
+   mkdir -d mongodb
+3. Run docker container
+   docker run -it -v mongo_db:/data/db -p 27017:27017 --name mongodb -d mongo
+4. Check docker container
+   docker ps
+5. You can also check logs
+   docker logs mongodb
